9becf8c41815400767db763dff68f16c19e507b2
lcs <cndtjq010@naver.com>
2021-06-22 09:05:13 +0900

    feat : delete posting function

--- blog/views.py
+++ blog/views.py
@@ -18,7 +18,6 @@ def post_new(request):
         form = PostForm(request.POST,request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
-            print(post.file)
             post.author= request.user
             post.published_date= timezone.now()
             post.save()
@@ -30,7 +29,7 @@ def post_new(request):
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method== "POST":
-        form = PostForm(request.POST, instance=post)
+        form = PostForm(request.POST,request.FILES, instance=post)
         if form.is_valid():
             post = form.save(commit=False)
             post.author= request.user
@@ -40,3 +39,9 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_delete(request, pk):
+    post = Post.objects.get(pk=pk)
+    post.delete()
+    return redirect('/')
